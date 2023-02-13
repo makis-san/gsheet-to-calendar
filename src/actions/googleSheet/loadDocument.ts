b43661ffa3abd0e7a71805af71a6f14082c0290b
Santiago Souza <santiagosouzape@gmail.com>
2023-02-13 00:11:19 -0300

    Remove console log

--- src/actions/googleSheet/loadDocument.ts
+++ src/actions/googleSheet/loadDocument.ts
@@ -3,7 +3,6 @@ import { GoogleSpreadsheet } from 'google-spreadsheet';
 
 export default async (docId: string, callback: (msg: string) => void) => {
   const doc = new GoogleSpreadsheet(docId);
-  console.log(process.env.DOC_API_KEY);
   doc.useApiKey(process.env.DOC_API_KEY as string);
 
   await doc.loadInfo().catch((error) => {
